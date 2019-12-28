379a16c3c5ffa22e382d8c760f9698c9e033d5f1
Demon <yuanyin@ideabinder.com>
2019-12-28 12:31:45 +0800

    .

--- app.js
+++ app.js
@@ -94,15 +94,18 @@ App({
                     success: res => {
                         // 通过 code 换取小程序 open_id
                         // 存储小程序 open_id
-                        // wx.setStorageSync('open_id', res.open_id);
+                        wx.setStorageSync('open_id', res.open_id);
                         // 储存 token，后面调接口放到 header 里
                         wx.setStorageSync('token', res.data.data.token||'');
+                      if (res.data.data.userInfo){
                         wx.setStorageSync('city', res.data.data.userInfo.city||'')
                         wx.setStorageSync('province', res.data.data.userInfo.province||'')
                         wx.setStorageSync('country', res.data.data.userInfo.country||'')
                         wx.setStorageSync('nickName', res.data.data.userInfo.nickName||'')
                         wx.setStorageSync('avatarUrl', res.data.data.userInfo.avatarUrl||'')
                         wx.setStorageSync('isAuthorization', res.data.data.userInfo||'')
+                      }
+                        
                     }
                 })
             } else {
@@ -176,12 +179,14 @@ App({
                         // wx.setStorageSync('open_id', res.open_id);
                         // 储存 token，后面调接口放到 header 里
                         wx.setStorageSync('token', res.data.data.token||'');
-                        wx.setStorageSync('city', res.data.data.userInfo.city||'')
-                        wx.setStorageSync('province', res.data.data.userInfo.province||'')
-                        wx.setStorageSync('country', res.data.data.userInfo.country||'')
-                        wx.setStorageSync('nickName', res.data.data.userInfo.nickName||'')
-                        wx.setStorageSync('avatarUrl', res.data.data.userInfo.avatarUrl||'')
-                        wx.setStorageSync('isAuthorization', res.data.data.userInfo||'')
+                      if (res.data.data.userInfo) {
+                        wx.setStorageSync('city', res.data.data.userInfo.city || '')
+                        wx.setStorageSync('province', res.data.data.userInfo.province || '')
+                        wx.setStorageSync('country', res.data.data.userInfo.country || '')
+                        wx.setStorageSync('nickName', res.data.data.userInfo.nickName || '')
+                        wx.setStorageSync('avatarUrl', res.data.data.userInfo.avatarUrl || '')
+                        wx.setStorageSync('isAuthorization', res.data.data.userInfo || '')
+                      }
                         cb && typeof cb == 'function' && cb();
                     }
                 })
